3c2e7b6edb8c1a9f77ba7d0e79c119f47c4d85cc
oportusgonzalo <oportusgonzalo@gmail.com>
2022-08-08 16:58:39 -0400

    Improves back propagation logic

--- back_propagation.py
+++ back_propagation.py
@@ -1,30 +1,42 @@
 import pandas as pd
-pd.set_option('display.max_columns', 15)
 
 # define parameters
 country = 'uk'
-parent_chain = 'booker'
+parent_chain = 'nisa'
 threshold_products = 85
 threshold_package = 75
+exists_canonical_catalog = True
 
 def main():
-    # reading file that has item_uuid and item_name match (source/raw file)
-    df_raw = pd.read_csv(f'data/{country}_{parent_chain}_uuid_name.csv')
+    if exists_canonical_catalog:
+        # reading file that has item_name and product_name (post NLP + regex cleaning)
+        df_back = pd.read_csv(f'back_propagation/raw_vs_clean_{country}_{parent_chain}_products_{threshold_products}_{threshold_package}.csv')
 
-    # reading file that has item_name and product_name (post NLP + regex cleaning)
-    df_post_nlp = pd.read_csv(f'back_propagation/raw_vs_clean_{country}_{parent_chain}_products_{threshold_products}_{threshold_package}.csv')
+        # reading file with direct matches
+        df_direct = pd.read_csv(f'bivariate_outputs/direct_matches_{country}_{parent_chain}_{threshold_products}_{threshold_package}.csv')
+        print(df_direct)
 
-    # reading file cleaned by agent task force
-    df_clean = pd.read_csv('agents_clean/clean_booker_UK.csv')
-    df_clean.columns = df_clean.columns.str.strip().str.lower()
+        # agents clean groups
+        df_clean = pd.read_csv(f'agents_clean/agents_clean_{country}_booker.csv') # add parameter parent_chain
+        df_clean.columns = df_clean.columns.str.strip().str.lower()
 
-    # DELETE
-    #print(f'% NAs: {round(df_clean[df_clean["label"].isna()].shape[0]/len(df_clean), 3)}')
-    df_clean.loc[df_clean['label'].isna(), 'label'] = 'not_yet'
-    df_clean['label'] = df_clean['label'].apply(lambda x: x.replace(' ', ''))
+        # Number of correct assignments
+        df_true = df_clean[~df_clean['label'].isna()]
+        df_true.label = df_true.label.str.strip().str.lower()
+        print(f'Number of correct assignments: {round(df_true[df_true.label == "true"].shape[0]/len(df_true), 3)}')
 
-    # stat of correct matches
-    print(f'Number of True matches: {round(df_clean[df_clean["label"] == "True"].shape[0]/len(df_clean), 4)}')
+        # useful columns
+        df_clean = df_clean.loc[: ,['member', 'canonical_leader', 'brand', 'name', 'package', 'promotion']]
+
+        # merging datasets
+        df_merge = df_back.merge(df_clean, how='inner', left_on='product_name', right_on='member')
+        df_merge.drop(['product_name', 'member'], axis=1, inplace=True)
+
+
+    print(df_back)
+    print(df_clean)
+    print(df_merge)
+    pass
 
     
 if __name__ == "__main__":
