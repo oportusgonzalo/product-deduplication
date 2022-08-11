ec7c0ad55e07f87c3c37db85db349f806754bdf0
oportusgonzalo <oportusgonzalo@gmail.com>
2022-08-11 12:52:02 -0400

    Fixes bugs in bivariate comparisson

--- bivariate_comparison.py
+++ bivariate_comparison.py
@@ -25,8 +25,8 @@ from nltk.corpus import stopwords
 # DEFINITIONS
 
 # parameters (MUST DO! -- are used to specify the outputs name)
-country = 'uk'
-parent_chain = 'nisa' # lower case and "clean"
+country = ''
+parent_chain = '' # lower case and "clean"
 item_column = 'item_name'
 language_ = 'en'
 
@@ -42,15 +42,11 @@ def read_and_select():
     print('Reading canonical and applicants files..')
     
     data = pd.read_csv(f'data/{country}_{parent_chain}_uuid_name.csv')
-
     # dict to map item_name with image_url:
     item_name_image_dict = dict(zip(data['item_name'], data['image_url']))
-
     data = data.loc[:, ['item_uuid', 'item_name']]
-    canonical_df = pd.read_csv(f'canonical_data/{canonical_file}.csv')
-    canonical_df.rename(columns={'group_id': 'canonical_id', 'leader': 'canonical_leader', 'member': 'canonical_member'}, inplace=True)
 
-    return data, canonical_df, item_name_image_dict
+    return data, item_name_image_dict
 
 def nlp_regex_cleaning(language_, data):
     print('NLP + Regex product name cleaning..')
@@ -67,10 +63,21 @@ def nlp_regex_cleaning(language_, data):
 
     return data_nlp.loc[:, ['item_uuid', 'item_name', 'product_name']]
 
-def direct_matches(data_nlp, canonical_df):
+def raw_vs_clean_name_mapping(df_nlp, item_name_image_dict): 
+    print('Saving file to back propagate matches..') 
+    df_back_propagation = df_nlp.loc[:, ['item_uuid', 'item_name', 'product_name']]
+    # adding image_url column
+    df_back_propagation['image_url'] = df_back_propagation['item_name'].map(item_name_image_dict)
+    clean_product_image_dict = dict(zip(df_back_propagation['product_name'], df_back_propagation['image_url']))
+    df_back_propagation.to_csv(f'back_propagation/raw_vs_clean_{country}_{parent_chain}_products_{threshold_products}_{threshold_package}.csv', index=False)
+    return df_back_propagation, clean_product_image_dict
+
+def direct_matches(data_nlp):
     print('Identifying direct matches: member --> canonical_member')
-    
-    canonical_members = list(set(canonical_df['canonical_member']))
+
+    # reading file with links between raw items and canonical data (when we run bivariate, this file has been already created)
+    canonical_links = pd.read_csv('canonical_data/canonical_links.csv')    
+    canonical_members = list(set(canonical_links['canonical_member']))
 
     # dataframe with direct matches
     direct_df = data_nlp[data_nlp['product_name'].isin(canonical_members)].reset_index(drop=True)
@@ -83,44 +90,37 @@ def direct_matches(data_nlp, canonical_df):
         data_not_direct = data_nlp[~data_nlp['product_name'].isin(direct_members)].reset_index(drop=True)
 
         # save link between: member --> canonical_member
-        direct_df = direct_df.merge(canonical_df, how='inner', left_on='product_name', right_on='canonical_member')
+        direct_df.drop(['item_uuid', 'item_name'], axis=1, inplace=True)
+        direct_df = direct_df.merge(canonical_links, how='inner', left_on='product_name', right_on='canonical_member')
         direct_matches_df = direct_df.loc[:, ['item_uuid', 'item_name', 'canonical_id', 'canonical_leader', 'canonical_member']]
+        direct_matches_df = direct_matches_df.drop_duplicates().reset_index(drop=True)
+
+        print(f'Validation - Number of existing matches: {len(direct_matches_df["canonical_member"].unique())}')
         
-        # return case: we have direct matches
-        return data_not_direct, canonical_df, direct_matches_df
     else:
         print(f'Number of existing matches: 0')
         # just to keep structrue
         data_not_direct = data_nlp.copy()
         direct_matches_df = pd.DataFrame() # --> empty DF (PROBABLY NOT USEFUL TO RETURN IT)
 
-        # return case: no direct matches
-        return data_not_direct, canonical_df, direct_matches_df
+    # return case: no direct matches
+    return data_not_direct, canonical_links, direct_matches_df
 
 
-def product_space_to_detect_similarities(data_not_direct, canonical_df):
+def product_space_to_detect_similarities(data_not_direct, canonical_links):
     print(f'Preparing set to identify similiarities by TF-IDF + Fuzzy..')
 
     applicants_not_direct = list(data_not_direct['product_name'].unique())
     # we use leaders as they reduce the space and represent the members
-    canonical_leaders = list(canonical_df[~canonical_df['canonical_leader'].isna()]['canonical_leader'].unique())
+    canonical_leaders = list(canonical_links[~canonical_links['canonical_leader'].isna()]['canonical_leader'].unique())
     # concatenation of: applicants with no direct match + canonical leaders
     product_space = list(set(applicants_not_direct + canonical_leaders))
-    print(f'Number of products in the set: {len(product_space)}')
+    print(f'Number of products to match and group: {len(product_space)}')
     return product_space
 
-def raw_vs_clean_name_mapping(df_nlp, item_name_image_dict): 
-    print('Saving file to back propagate matches..') 
-    df_back_propagation = df_nlp.loc[:, ['item_uuid', 'item_name', 'product_name']]
-    # adding image_url column
-    df_back_propagation['image_url'] = df_back_propagation['item_name'].map(item_name_image_dict)
-    clean_product_image_dict = dict(zip(df_back_propagation['product_name'], df_back_propagation['image_url']))
-    df_back_propagation.to_csv(f'back_propagation/raw_vs_clean_{country}_{parent_chain}_products_{threshold_products}_{threshold_package}.csv', index=False)
-    return df_back_propagation, clean_product_image_dict
-
-def leaders_lead(canonical_df, groups_df):
+def leaders_lead(canonical_links, groups_df):
     print(f'Making sure leaders are leaders..')
-    canonical_leaders = canonical_df['canonical_leader'].unique()
+    canonical_leaders = canonical_links['canonical_leader'].unique()
     # identifying all groups where canonical members are present
     canonical_leaders_group_df = groups_df.loc[groups_df['member'].isin(canonical_leaders)][['group_id', 'member']].drop_duplicates().reset_index(drop=True)
     # dict to replace: group leader by canonical_leader
@@ -130,6 +130,8 @@ def leaders_lead(canonical_df, groups_df):
     # replace canonical leaders in potential group leader column
     for group_id, leader in canonical_leader_replace_dict.items():
         groups_df.loc[groups_df['group_id'] == group_id, ['leader', 'modify_leader']] = leader, 'No'
+    # removing canonical leaders from members --> may lead to issue (leaders being mapped to other leaders)
+    groups_df = groups_df[~groups_df['member'].isin(canonical_leaders)].copy()
     return groups_df
 
 
@@ -137,15 +139,15 @@ def main():
     # Initial time
     t_initial = gets_time()
     # reading CSV files: canonical & applicnats
-    data, canonical_df, item_name_image_dict = read_and_select()
+    data, item_name_image_dict = read_and_select()
     # NLP + regex product name cleaning --> new column: product_name
     data_nlp = nlp_regex_cleaning(language_, data)
     # saving raw product name - clean product name (post NLP + regex): mapping
     df_back_propagation, clean_product_image_dict = raw_vs_clean_name_mapping(data_nlp, item_name_image_dict)
     # Identifying direct matches: member --> canonical_member
-    data_not_direct, canonical_df, direct_matches_df = direct_matches(data_nlp, canonical_df)
+    data_not_direct, canonical_links, direct_matches_df = direct_matches(data_nlp)
     # Preparing set to run grouping script
-    product_space = product_space_to_detect_similarities(data_not_direct, canonical_df)
+    product_space = product_space_to_detect_similarities(data_not_direct, canonical_links)
     df_product_space = pd.DataFrame(data={'product_name': product_space})
     
     # Appying TF-IDF method
@@ -166,7 +168,7 @@ def main():
     groups_df, track_df = groups_concatenation(df_clean, df_similars, index_product_dict)
 
     # leaders lead
-    groups_df = leaders_lead(canonical_df, groups_df)
+    groups_df = leaders_lead(canonical_links, groups_df)
     
     # saving results
     groups_df = groups_df.sort_values(by=['leader', 'member']).reset_index(drop=True)
