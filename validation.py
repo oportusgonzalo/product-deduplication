71acee6c4eef5a9f2067c3faf76e8c003d1c151d
oportusgonzalo <oportusgonzalo@gmail.com>
2022-09-23 14:37:23 -0300

    Adds uk keresley

--- validation.py
+++ validation.py
@@ -66,7 +66,7 @@ def main(country, country_stores_dict):
 if __name__ == "__main__":
     
     country_stores_dict = {
-        'uk': ['booker', 'nisa', 'costcutter', 'bestway', 'rev_nisa', 'rev_costcutter', 'rev_bestway', 'rev_booker'],
+        'uk': ['booker', 'nisa', 'costcutter', 'bestway', 'rev_nisa', 'rev_costcutter', 'rev_bestway', 'rev_booker', 'keresley_news'],
         'cr': ['dp&az', 'ampm', 'fresh_market', 'rev_dp&az', 'rev_ampm', 'rev_fresh_market', 'farmavalue', 'fciabombacr', 'dos_pinos_cr']
     }
 
