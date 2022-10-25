bd6995c17b36c87f5590a47fd580e8854880034e
oportusgonzalo <oportusgonzalo@gmail.com>
2022-10-25 15:23:03 -0300

    Adds merchants

--- validation.py
+++ validation.py
@@ -66,9 +66,9 @@ def main(country, country_stores_dict):
 if __name__ == "__main__":
     
     country_stores_dict = {
-        'uk': ['booker', 'nisa', 'costcutter', 'bestway', 'rev_nisa', 'rev_costcutter', 'rev_bestway', 'rev_booker', 'keresley_news', 'tulsi_news', 'all_in_hornchurch', 'cricklewood', 'montrose_street'],
+        'uk': ['booker', 'nisa', 'costcutter', 'bestway', 'rev_nisa', 'rev_costcutter', 'rev_bestway', 'rev_booker', 'keresley_news', 'tulsi_news', 'all_in_hornchurch', 'cricklewood', 'montrose_street', 'fmcg1'],
         'cr': ['dp&az', 'ampm', 'fresh_market', 'rev_dp&az', 'rev_ampm', 'rev_fresh_market', 'farmavalue', 'fciabombacr', 'dos_pinos_cr', 'musicr', 'peqmundo'],
-        'za': ['brands1']
+        'za': ['brands1', 'brands2']
     }
 
     for country in ['uk', 'cr', 'za']:
